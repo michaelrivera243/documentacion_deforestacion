306dd1b04175b1099eeff5d79e8c00fee238c7e6
michaelrivera243 <michael.rivera243@gmail.com>
2023-08-29 19:29:57 -0500

    modify ducusaurus.config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,15 +11,17 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://michaelrivera243.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/documentacion_deforestacion/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'michaelrivera243', // Usually your GitHub org/user name.
+  projectName: 'documentacion_deforestacion', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
